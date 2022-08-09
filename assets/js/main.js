a56ddfb8e4cc18cb4536ce33c54552340a82c0d1
mmoraisjr <jr.morais.marcos@gmail.com>
2022-08-09 18:31:34 -0300

    update in function / add tag buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 const inputPrice = document.querySelector(".total-price");
 const inputPeople = document.querySelector(".people");
-const inputTip = document.querySelector(".tip");
 const btn = document.querySelector(".imprimir");
 const exibe = document.querySelector("#demo");
+const inputTip = document.querySelectorAll(".buttons");
 
 var price;
 var tip;
@@ -10,23 +10,36 @@ var people;
 var total;
 var totalPerPerson;
 
+inputTip.forEach((elemento) => {
+  elemento.addEventListener("click", (e) => {
+    tip = e.target.value;
+  });
+});
+
 btn.addEventListener("click", () => inputs());
 
 const inputs = () => {
   price = inputPrice.value;
   people = inputPeople.value;
-  tip = inputTip.value;
+  ok = btn.value;
 
   calcTip(price, people, tip);
+
+  exibe.innerHTML =
+    "Valor: " +
+    price +
+    "<br>Porcentagem de gorjeta: " +
+    tip +
+    "%" +
+    "<br>Pessoas na mesa: " +
+    people +
+    "<br>Valor Total: " +
+    total +
+    "<br>Valor Total por Pessoa: " +
+    totalPerPerson;
 };
 
 function calcTip(price, people, tip) {
   total = parseInt(price) + (parseInt(price) * parseInt(tip)) / 100;
   totalPerPerson = parseInt(total) / parseInt(people);
-  exibe.innerHTML =
-    "Valor: " + price +
-    "<br>Porcentagem de gorjeta: " + tip + "%" +
-    "<br>Pessoas na mesa: " + people +
-    "<br>Valor Total: " + total +
-    "<br>Valor Total por Pessoa: " + totalPerPerson;
 }
